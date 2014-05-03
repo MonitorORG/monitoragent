4ea54d214213e24f692c476cc8a1060c257fd686
kailong <kailong.wu@163.com>
2014-05-03 19:55:56 +0800

    update Agent.zip file

--- Agent/startup.bat
+++ Agent/startup.bat
@@ -1,2 +1,2 @@
-:call java -jar -Djava.library.path=.\libdll HostAgent-1.jar
-start cmd /c java -jar -Djava.library.path=.\libdll HostAgent-1.jar
+:call java -jar -Djava.library.path=.\libdll agent.jar
+start cmd /c java -jar -Djava.library.path=.\libdll agent.jar
